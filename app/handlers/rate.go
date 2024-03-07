cde1408e1d6c2e8a9c2ac8eec3e55adde5142b0a
Jack Dunlap <jdunlap@givens.com>
2024-03-07 08:32:05 -0500

    Update rate.go

--- app/handlers/rate.go
+++ app/handlers/rate.go
@@ -129,7 +129,7 @@ func PostRequestWithContext(ctx context.Context, client *http.Client, url string
 	if resp.StatusCode == http.StatusOK {
 		log.Info("Status Code: %v, [UUID: %v] [StopID: %d] | Response %s", resp.StatusCode, requestID, stopID, responseBody)
 	} else {
-		log.Error(string(responseBody))
+		log.Error("Status Code: %v",resp.StatusCode)
 
 	}
 
